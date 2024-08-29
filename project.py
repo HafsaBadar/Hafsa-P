85508c0b93fd2c9bb4f570d03a0c694d7d8f2c50
HafsaBadar <hbadar.phdabs21asab@student.nust.edu.pk>
2024-08-29 13:08:37 +0500

    Update project.py

--- project.py
+++ project.py
@@ -10,7 +10,6 @@ Original file is located at
 # Import necessary libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
